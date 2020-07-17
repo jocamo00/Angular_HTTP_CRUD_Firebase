98acaf66dafc4a92a5358f3568862b9485f9cd79
Jose Carreres <jose.carreres.81@gmail.com>
2020-07-17 16:39:35 +0200

    Creacion de los componentes heroe y heroes y sus rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
+
+// Rutas
 import { Routes, RouterModule } from '@angular/router';
 
+// Componentes
+import { HeroesComponent } from './pages/heroes/heroes.component';
+import { HeroeComponent } from './pages/heroe/heroe.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'heroe/:id', component: HeroeComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'heroes' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
