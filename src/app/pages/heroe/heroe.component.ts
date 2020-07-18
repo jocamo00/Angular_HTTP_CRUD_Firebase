3e46bd7938f356055bad1f6b902b196f5f456cd9
Jose Carreres <jose.carreres.81@gmail.com>
2020-07-18 02:53:24 +0200

    Diseño del componente heroe.component.html

--- src/app/pages/heroe/heroe.component.ts
+++ src/app/pages/heroe/heroe.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faArrowLeft, faSmileWink, faDizzy, faSave } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-heroe',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroe.component.css']
 })
 export class HeroeComponent implements OnInit {
+
+  faArrowLeft = faArrowLeft;
+  faSmileWink = faSmileWink;
+  faDizzy = faDizzy;
+  faSave = faSave;
 
   constructor() { }
 
